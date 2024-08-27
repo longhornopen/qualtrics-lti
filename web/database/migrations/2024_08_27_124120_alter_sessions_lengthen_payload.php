58f7efb7143046970a1510b76b5a0b216dfa2b7f
Chris Pittman <chrispittman73@gmail.com>
2024-08-27 07:42:13 -0500

    update session payload length

---
+++ web/database/migrations/2024_08_27_124120_alter_sessions_lengthen_payload.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Laravel now uses a longText column for the session payload by default
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->longText('payload')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->text('payload')->change();
+        });
+    }
+};

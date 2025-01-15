be19e0105a157d0756821ab44718871cff3a20ca
Thang Truong <40693511+BrianTruong23@users.noreply.github.com>
2025-01-15 08:51:14 -0600

    add messages.php and call that in app controller (#28)
    * add messages.php and call that in app controller
    
    * change key to default_intro_text and default_finish_text
    
    ---------
    
    Co-authored-by: Thang Truong <thangtruong@wireless-10-146-45-130.public.utexas.edu>

---
+++ web/resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+ 
+// lang/en/messages.php
+ 
+return [
+    'default_intro_text' => 'Welcome!  Please click below to begin your survey.',
+    'default_finish_text' => 'Thank you for taking this survey!  Your response has been recorded.',
+];
